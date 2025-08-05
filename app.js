3ca8f98ec19da406794d9ecdf2aa48a82dbc6a1e
VanMicha <52376877+VanMicha@users.noreply.github.com>
2025-08-05 21:29:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function render() {
   if (solved.length === riddles.length && solved.every(Boolean)) {
     const solution = document.createElement("div");
     solution.className = "solution";
-    solution.innerText = "Nutze purple um die korrekte Schrift zu finden";
+    solution.innerText = "Du hast es geschafft: Nutze purple um die korrekte Schrift zu finden";
     app.appendChild(solution);
   }
 
